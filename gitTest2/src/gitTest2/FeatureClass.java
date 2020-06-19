7a3b1a7c05b0ffc857d11a7c171a921a34951e26
User <User@HP>
2020-06-20 00:29:39 +0530

    newwwww

--- gitTest2/src/gitTest2/FeatureClass.java
+++ gitTest2/src/gitTest2/FeatureClass.java
@@ -2,6 +2,6 @@ package gitTest2;
 
 public class FeatureClass {
 public static void main(String[] args) {
-	System.out.println("This is feature1 local branch");
+	System.out.println("This is feature1 local branch newwwww");
 }
 }
